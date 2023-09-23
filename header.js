d5d969c79b00f0cca51128b2b64162eed479200f
KAYRAZZ <valentin.chigot@orange.fr>
2023-09-23 23:29:28 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // document.getElementById("loader").style.display = "none";
+
+    const liens = document.querySelectorAll(".lien");
+    const currentPageURL = window.location.href;
+    const gradientAnimation = document.querySelector(".gradientAnimation");
+    const body = document.body;
+    setTimeout(function () {
+        body.style.opacity = 1;
+        gradientAnimation.style.opacity = 0.3;
+    }, 500);
+
+    liens.forEach((lien) => {
+        const linkURL = lien.href;
+        if (linkURL === currentPageURL || (currentPageURL + 'index.html') === linkURL) {
+            setTimeout(function () {
+                lien.classList.add("active");
+            }, 500);
+        }
+
+        lien.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (lien.href != currentPageURL) {
+                gradientAnimation.style.opacity = 0;
+                body.style.opacity = 0;
+                liens.forEach((lienActive) => {
+                    var a = lienActive.getAttribute("class");
+                    if (a == 'lien active') {
+                        lienActive.classList.remove("active");
+                    }
+                });
+                // Attendre 2 secondes avant le changement de page
+                setTimeout(function () {
+                    // Rediriger vers la nouvelle page
+                    window.location.href = lien.href;
+                }, 300);
+            }
+        })
+    });
+});
+
